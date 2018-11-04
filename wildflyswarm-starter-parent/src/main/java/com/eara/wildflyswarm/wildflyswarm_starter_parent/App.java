ef9d3834042cf6acc69dfd18b5ea1332c0720216
Ernesto A. Rodriguez Acosta <ernestoacostacuba@outlook.it>
2018-11-04 12:53:31 +0100

    Adding wildflyswarm-starter-parent

---
+++ wildflyswarm-starter-parent/src/main/java/com/eara/wildflyswarm/wildflyswarm_starter_parent/App.java
@@ -0,0 +1,13 @@
+package com.eara.wildflyswarm.wildflyswarm_starter_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
