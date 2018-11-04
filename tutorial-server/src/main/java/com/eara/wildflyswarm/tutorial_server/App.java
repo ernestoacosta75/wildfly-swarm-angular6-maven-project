48b7624c5db594d2d39b20398d20eeaf29e3d026
Ernesto A. Rodriguez Acosta <ernestoacostacuba@outlook.it>
2018-11-04 13:15:52 +0100

    adding server module

---
+++ tutorial-server/src/main/java/com/eara/wildflyswarm/tutorial_server/App.java
@@ -0,0 +1,13 @@
+package com.eara.wildflyswarm.tutorial_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
